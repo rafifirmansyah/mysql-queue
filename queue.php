2d93d7a95a49d2b3f2f84cfed623de4274ffff47
Sebastian Jennen <sebastian.jennen@gmx.de>
2016-10-11 19:30:03 +0200

    first draft of php class

---
+++ queue.php
@@ -0,0 +1,17 @@
+<?php
+class Queue
+{
+	public void __construct(){
+		// TODO mysql connection
+	}
+
+	public void push($payload){
+		// TODO db call
+	}
+
+	public void pop(){
+		// TODO db call
+		return 'payload';
+	}
+}
+?>
