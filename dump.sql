ed2bab4bceb8fb30451541e3589ba2063bae1f45
Sebastian Jennen <sebastian.jennen@gmx.de>
2016-12-26 09:04:26 +0100

    fix popJob logic

---
+++ dump.sql
@@ -0,0 +1,141 @@
+-- MySQL dump 10.16  Distrib 10.1.17-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: queue
+-- ------------------------------------------------------
+-- Server version	10.1.17-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `queue`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `queue` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `queue`;
+
+--
+-- Table structure for table `job`
+--
+
+DROP TABLE IF EXISTS `job`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `job` (
+  `idJob` int(11) NOT NULL AUTO_INCREMENT,
+  `clientId` int(11) DEFAULT NULL,
+  `payload` text,
+  `status` enum('new','processing','done','error') DEFAULT 'new',
+  `updated` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `created` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idJob`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `job`
+--
+
+LOCK TABLES `job` WRITE;
+/*!40000 ALTER TABLE `job` DISABLE KEYS */;
+/*!40000 ALTER TABLE `job` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'queue'
+--
+/*!50003 DROP FUNCTION IF EXISTS `markJob` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `markJob`(
+	VjobId INT,
+	Vstatus enum('new', 'processing', 'done', 'error')
+) RETURNS int(11)
+BEGIN	
+/* markJob - A procedure to mark a job with a status */	
+	UPDATE job SET 
+		status = Vstatus
+        	WHERE idJob = VjobId;
+	RETURN AFFECTED_ROWS();
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP FUNCTION IF EXISTS `pushJob` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `pushJob`(Vpayload TEXT) RETURNS int(11)
+BEGIN
+/* pushJob - a function to create a new job*/
+	INSERT IGNORE INTO job (payload) VALUES (Vpayload);
+ 	RETURN LAST_INSERT_ID();
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `popJob` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `popJob`(VclientId INT)
+BEGIN	
+/* popJob - A procedure to request the next 'new' job */
+	UPDATE job SET 
+		clientId = VclientId
+        	WHERE status = 'processing'
+        	ORDER BY idJob ASC LIMIT 1;
+
+	SELECT idJob, created, payload
+		FROM job
+		WHERE clientId = VclientId
+        	AND status = 'processing' LIMIT 1;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-10-12 10:59:09
--- queue.sql
+++ queue.sql
@@ -3,17 +3,25 @@ DROP DATABASE IF EXISTS queue;
 CREATE DATABASE queue;
 USE queue;
 
-/* create table jobs */
-DROP TABLE IF EXISTS job;
-CREATE TABLE job(
+/* create table client */
+CREATE OR REPLACE TABLE client(
+	idClient INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(50),
+	updated DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+	created DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+/* create table job */
+CREATE OR REPLACE TABLE job(
 	idJob INT AUTO_INCREMENT PRIMARY KEY,
 	clientId INT,
 	payload TEXT,
 	status enum('new', 'processing', 'done', 'error') DEFAULT 'new',
 	updated DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-	created DATETIME DEFAULT CURRENT_TIMESTAMP
+	created DATETIME DEFAULT CURRENT_TIMESTAMP,
+	FOREIGN KEY (clientId) REFERENCES client(idClient)
+		ON UPDATE CASCADE ON DELETE SET NULL
 );
-
 
 DELIMITER $$
 CREATE OR REPLACE FUNCTION pushJob (Vpayload TEXT)
@@ -30,8 +38,9 @@ CREATE OR REPLACE PROCEDURE popJob (VclientId INT)
 BEGIN	
 /* popJob - A procedure to request the next 'new' job */
 	UPDATE job SET 
-		clientId = VclientId
-        	WHERE status = 'processing'
+		clientId = VclientId,
+		status = 'processing',
+        	WHERE status = 'new'
         	ORDER BY idJob ASC LIMIT 1;
 
 	SELECT idJob, created, payload
